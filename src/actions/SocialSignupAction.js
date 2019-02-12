098a83270681da3c1ec31b970bc5a51c91cd254e
muhwezi deo <aggrey256@gmail.com>
2019-02-12 15:16:57 +0300

    add console.log to view response

--- src/actions/SocialSignupAction.js
+++ src/actions/SocialSignupAction.js
@@ -3,6 +3,7 @@ import {postDataNoAuth,baseURL} from '../constants';
 
 
 const facebookSignUp=(accessToken)=>(dispatch)=>{
+    console.log(accessToken)
     postDataNoAuth(`${baseURL}/api/users/facebook-login/`,accessToken).then(
         data=>{
             console.log(data)
