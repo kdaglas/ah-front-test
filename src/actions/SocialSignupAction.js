2f447e086b3273fb2545608b51f2c562df4e3e64
muhwezi deo <aggrey256@gmail.com>
2019-02-12 15:38:17 +0300

    fix body for token data

--- src/actions/SocialSignupAction.js
+++ src/actions/SocialSignupAction.js
@@ -4,7 +4,7 @@ import {postDataNoAuth,baseURL} from '../constants';
 
 const facebookSignUp=(accessToken)=>(dispatch)=>{
     console.log(accessToken)
-    postDataNoAuth(`${baseURL}/api/users/facebook-login/`,accessToken).then(
+    postDataNoAuth(`${baseURL}/api/users/facebook-login/`,{"auth_token":accessToken}).then(
         data=>{
             console.log(data)
         }
